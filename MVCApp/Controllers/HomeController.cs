1a0f97a32afae771cc2dda7916a326591955abcb
Drake Scott <dascott0204@gmail.com>
2017-04-17 23:38:38 -0500

    Implemented JsonResult, Content and ObjectResult return types.

--- MVCApp/Controllers/HomeController.cs
+++ MVCApp/Controllers/HomeController.cs
@@ -3,23 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MVCApp.Models;
 
 namespace MVCApp.Controllers
 {
-    [Route("[Controller]")]
+    
     public class HomeController : Controller
     {
-        [Route("[Action]")]
+       
         public IActionResult Index()
         {
             return View();
         }
 
 
-        [Route("[Action]")]
-        public string IdRoute()
+        
+        public ContentResult IdRoute()
         {
-            return "Attribute Routing";
+            return Content("Attribute Routing");
+        }
+
+        //Return Types
+        public JsonResult MyObject()
+        {
+            var mymodel = new MyData{ MyId = 1, MyValue = "My first Value"};
+
+            return Json(mymodel);
         }
     }
 }
---
+++ MVCApp/Models/MyData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApp.Models
+{
+    public class MyData
+    {
+        public int MyId { get; set; }
+
+        public string MyValue { get; set; }
+
+    }
+}
