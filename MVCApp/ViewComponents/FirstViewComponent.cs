c784da740cedf8c0e6e91893b6d500994b88d2ce
Drake Scott <dascott0204@gmail.com>
2017-04-18 04:49:40 -0500

    View Components in MVC 6

---
+++ MVCApp/ViewComponents/FirstViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCApp.Services;
+
+namespace MVCApp.ViewComponents
+{
+    public class FirstViewComponent:ViewComponent
+    {
+        private IMyInjectedService myService;
+
+        public FirstViewComponent(IMyInjectedService service)
+        {
+            myService = service;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var myItem = await GetGuid();
+            return View("Default", myItem);
+        }
+
+        public async Task<IMyInjectedService> GetGuid()
+        {
+            return await Task.FromResult(myService);
+        }
+    }
+}
